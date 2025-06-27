f1dc05fce054ff53d7dfe211c15a84192ccfb1ca
Lopez Gonzalo <gonzalo@smtv.dev>
2025-06-27 16:47:20 -0300

    style: enhance error comments for clarity in ISecuritizeOffRampErrors interface

--- contracts/off-ramp/ISecuritizeOffRampErrors.sol
+++ contracts/off-ramp/ISecuritizeOffRampErrors.sol
@@ -18,14 +18,44 @@
 pragma solidity ^0.8.22;
 
 interface ISecuritizeOffRampErrors {
+    /*
+     * @dev error selector: 0xeac0d389
+     */
     error ZeroAddress(string parameter);
+    /*
+     * @dev error selector: 0x899c84b8
+     */
     error RateNotDefined();
+    /*
+     * @dev error selector: 0xa952e4e7
+     */
     error InsufficientRedeemerBalance(address redeemer, uint256 requested, uint256 available);
+    /*
+     * @dev error selector: 0xa12d8719
+     */
     error RestrictedCountry(string country);
+    /*
+     * @dev error selector: 0xa17e11d5
+     */
     error InsufficientLiquidity(uint256 requested, uint256 available);
+    /*
+     * @dev error selector: 0x4d78a3cb
+     */
     error EmptyCountryCode();
+    /*
+     * @dev error selector: 0x5179a003
+     */
     error InvalidCountryCodeLength(uint256 length);
+    /*
+     * @dev error selector: 0x3fc25467
+     */
     error NonUppercaseCountryCode(uint8 index, bytes1 character);
+    /*
+     * @dev error selector: 0xfef38516
+     */
     error ExcessiveDecimals(uint256 decimals, uint256 maxDecimals);
+    /*
+     * @dev error selector: 0xd28d3eb5
+     */
     error InsufficientOutputAmount(uint256 outputAmount, uint256 minOutputAmount);
 }
